e8ca03b1d6389c5a9312e09ae4811d3935dfee01
ErythroME <megas.nmsis@hotmail.com>
2017-03-27 13:29:04 +0800

    Make jest happy

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,23 @@
-import 'react-native';
-import React from 'react';
-import Index from '../index.ios.js';
+import 'react-native'
+import React from 'react'
+import App from '../app/App.js'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
+import { mock, release } from 'mock-async-storage'
+import { AsyncStorage as storage } from 'react-native'
+
+
+mock()
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
-});
+    <App />
+  )
+})
+
+it('Mock Async Storage working', async () => {
+  await storage.setItem('myKey', 'myValue')
+  const value = await storage.getItem('myKey')
+  expect(value).toBe('myValue')
+})
